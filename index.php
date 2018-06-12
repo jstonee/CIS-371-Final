e1cb94c1d347067c3c6992f3cd241e6a4dd5cf95
Jonathan Stone <stonejo@mail.gvsu.edu>
2018-06-12 16:20:09 +0000

    Not using the ratings

--- index.php
+++ index.php
@@ -4,45 +4,14 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Rate an Assignment</title>
-  <link href="css/rating.css" rel="stylesheet" type="text/class">
   <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
   <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
   <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-  <script type="text/javascript" src="js/rating.js"></script>
 <script>
   $( function() {
     $( "#accordion" ).accordion();
   });
 </script>
-<!--
-<script language="javascript" type="text/javascript">
-$(function() {
-    $("#rating_star").spaceo_rating_widget({
-	starLength: '5',
-	initialValue: '',
-	callbackFunctionName: 'processRating',
-	imageDirectory: 'img/',
-	inputAttr: 'post_id'
-    });
-  });
-  function processRating(val, attrVal){
-	$.ajax({
-		type: 'POST',
-		url: 'rating.php',
-		data: 'post_id=1&points='+val,
-		dataType: 'json',
-		success: function(data) {
-			if (data.status == 'ok') {
-				alert('You have rated '+val);
-			}else {
-				alert('Please try again in a minute.');
-			}
-		}
-	});
-    }
-  </script>
--->
 </head>
 <body>
 
@@ -119,7 +88,6 @@ foreach($c as $row)
 		echo "<div><p><h4>Assignment ID: " . $row->id . "</h4>";
 		echo "<textarea name='ta" . $i . "' rows='4' cols='50'></textarea>";
 		echo "<input type='submit' name='sub" . $i . "' /></p></div>";
-		//echo "<input name='rating" . $i . "' value='0' id='rating_star' type='hidden' postID='1'/></p></div>";
 	}
 	$i++;
 }
